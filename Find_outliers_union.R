e9def31bd3f7fa4eacdd61d2cd4839ecd3e2b437
giorgiaauteri <42116272+giorgiaauteri@users.noreply.github.com>
2019-07-10 11:10:47 -0400

    Update and rename IndexThreeOutliers.R to Find_outliers_union.R

--- Find_outliers_union.R
+++ Find_outliers_union.R
@@ -1,18 +1,18 @@
 #Read in outliers identified via different methods and then see which SNPs are the same
 
 #Read in outlier SNPs from STACKS
-sel <- read.csv("D:/Mirror/Mirror/Projects/PhD Thesis/SurvMortWNS/Data/SurvMort21/Pops_SMAll/SelectedLoci.csv")
+sel <- read.csv("SelectedLoci.csv")
 sel$CHROM <- as.character(sel$CHROM)
 sel$POS <- as.numeric(sel$POS)
 
 #Read in outliers from diveRsity
-sel2 <- read.csv("D:/Mirror/Mirror/Projects/PhD Thesis/SurvMortWNS/Data/SurvMort21/Bootstrap/bootstrap3/bootstrapOutliersOver5SD.csv")
+sel2 <- read.csv("bootstrapOutliersOver5SD.csv")
 colnames(sel2) <- c("Locus", "diversityFST", "lower", "upper", "CHROM", "POS")
 sel2$CHROM <- as.character(sel2$CHROM)
 sel2$POS <- as.numeric(sel2$POS)
 
 #Read in outliers from outFLANK
-sel3 <- read.csv("D:/Mirror/Mirror/Projects/PhD Thesis/SurvMortWNS/Data/SurvMort21/Pops_SMAll/SelectedLoci_OutFlank.csv")
+sel3 <- read.csv("SelectedLoci_OutFlank.csv")
 sel3 <- sel3[ ,c(2:5, 8, 11:15)]
 colnames(sel3) <- c("CHROM", "POS", "He_OF", "FST_OF", "FSTnoCorr_OF", 
                     "meanAlleleFreq_OF", "pvalues_OF", "rightTail_OF",
@@ -34,9 +34,9 @@ final <- na.omit(final)
 write.csv(final,  
            quote = FALSE,
           row.names = FALSE,
-           file="D:/Mirror/Mirror/Projects/PhD Thesis/SurvMortWNS/Data/SurvMort21/Pops_SMAll/SelectedLoci_PreIDs.csv")
+           file="SelectedLoci_PreIDs.csv")
 ##General candidates 
 write.csv(generalCandidates,  
           quote = FALSE,
           row.names = FALSE,
-          file="D:/Mirror/Mirror/Projects/PhD Thesis/SurvMortWNS/Data/SurvMort21/Pops_SMAll/LociOfInterest_PreIDs.csv")
+          file="LociOfInterest_PreIDs.csv")
